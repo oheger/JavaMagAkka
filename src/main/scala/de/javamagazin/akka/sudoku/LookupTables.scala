bffc03e80b08b76d409bf8a47d4845d3b608663e
oheger <oliver.heger@oliver-heger.de>
2017-04-30 21:43:36 +0200

    Added lookup table classes.
    These classes store information about occupied cells that is
    needed during Sudoku verification and solving.

---
+++ src/main/scala/de/javamagazin/akka/sudoku/LookupTables.scala
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2017 Oliver Heger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.javamagazin.akka.sudoku
+
+/**
+  * A class storing information about occupied cells.
+  *
+  * This information is used during Sudoku solving. It allows a fast check
+  * whether a number can be placed at a specific position.
+  *
+  * The information consists of several 2-dimensional boolean arrays. The
+  * first index in such an array selects a number. The second index references
+  * a specific position (row, column, or square). A value of '''true''' means
+  * that this number is already present at this position.
+  *
+  * Instances of this class are created from [[MutableLookupTables]] objects.
+  */
+class LookupTables private[sudoku](val squareSize: Int,
+                                   filledRows: Array[Array[Boolean]],
+                                   filledColumns: Array[Array[Boolean]],
+                                   filledSquares: Array[Array[Boolean]]) {
+  /**
+    * Returns a mutable lookup table initialized from the data stored in
+    * this instance.
+    *
+    * @return the ''MutableLookupTables'' based on this object
+    */
+  def toMutable: MutableLookupTables =
+    MutableLookupTables(squareSize, filledRows, filledColumns, filledSquares)
+}
+
+object MutableLookupTables {
+  /**
+    * Creates a new instance of ''MutableLookupTables'' with unused cells
+    * of the specified square size.
+    *
+    * @param squareSize the square size
+    * @return the new instance
+    */
+  def apply(squareSize: Int): MutableLookupTables = {
+    val fieldSize = squareSize * squareSize
+    new MutableLookupTables(squareSize, Array.ofDim[Boolean](fieldSize, fieldSize),
+      Array.ofDim[Boolean](fieldSize, fieldSize),
+      Array.ofDim[Boolean](fieldSize, fieldSize))
+  }
+
+  /**
+    * Creates a new instance of ''MutableLookupTables'' with the specified
+    * data.
+    *
+    * @param squareSize    the square size
+    * @param filledRows    the array for filled rows
+    * @param filledColumns the array for filled columns
+    * @param filledSquares the array for filled squares
+    * @return the new instance
+    */
+  def apply(squareSize: Int, filledRows: Array[Array[Boolean]],
+            filledColumns: Array[Array[Boolean]],
+            filledSquares: Array[Array[Boolean]]): MutableLookupTables =
+    new MutableLookupTables(squareSize, cloneArray(filledRows), cloneArray(filledColumns),
+      cloneArray(filledSquares))
+}
+
+/**
+  * A class storing information about occupied cells and allowing to manipulate
+  * this data.
+  *
+  * This class is used internally during Sudoku verification and solving.
+  *
+  * @param squareSize    the square size
+  * @param filledRows    the array for filled rows
+  * @param filledColumns the array for filled columns
+  * @param filledSquares the array for filled squares
+  */
+class MutableLookupTables private(val squareSize: Int,
+                                  filledRows: Array[Array[Boolean]],
+                                  filledColumns: Array[Array[Boolean]],
+                                  filledSquares: Array[Array[Boolean]]) {
+  def isRowUsed(value: Int, row: Int): Boolean = filledRows(value - 1)(row)
+
+  def isColumnUsed(value: Int, col: Int): Boolean = filledColumns(value - 1)(col)
+
+  def isSquareUsed(value: Int, idx: Int): Boolean = filledSquares(value - 1)(idx)
+
+  def isSquareUsedAt(value: Int, row: Int, col: Int): Boolean =
+    isSquareUsed(value, getSquareIndex(row, col))
+
+  /**
+    * Sets the used flag for a value at the specified position. This means
+    * that this number is placed at this position which restricts other
+    * placements according to Sudoku rules.
+    *
+    * @param value the value
+    * @param row   the row index
+    * @param col   the column index
+    * @param used  flag whether the number should be placed or removed
+    */
+  def markUsed(value: Int, row: Int, col: Int, used: Boolean): Unit = {
+    val valIdx = value - 1
+    filledRows(valIdx)(row) = used
+    filledColumns(valIdx)(col) = used
+    filledSquares(valIdx)(getSquareIndex(row, col)) = used
+  }
+
+  /**
+    * Returns a ''LookupTables'' object with the data of this instance.
+    *
+    * @return the ''LookupTables'' object
+    */
+  def toImmutable: LookupTables =
+    new LookupTables(squareSize, cloneArray(filledRows), cloneArray(filledColumns),
+      cloneArray(filledSquares))
+
+  /**
+    * Determines the index for a square based on the specified position.
+    *
+    * @param row the row index
+    * @param col the column index
+    * @return the index of the corresponding square
+    */
+  private def getSquareIndex(row: Int, col: Int): Int =
+    (row / squareSize) * squareSize + col / squareSize
+}
---
+++ src/test/scala/de/javamagazin/akka/sudoku/MutableLookupTablesSpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2017 Oliver Heger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.javamagazin.akka.sudoku
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''MutableLookupTables''.
+  */
+class MutableLookupTablesSpec extends FlatSpec with Matchers {
+  "A MutableLookupTables" should "produce an empty instance" in {
+    val tab = MutableLookupTables(3)
+    for {v <- 1 to 9
+         i <- 0 until 9
+    } {
+      tab.isRowUsed(v, i) shouldBe false
+      tab.isColumnUsed(v, i) shouldBe false
+      tab.isSquareUsed(v, i) shouldBe false
+    }
+  }
+
+  it should "produce an instance from existing arrays" in {
+    val rows = Array.ofDim[Boolean](4, 4)
+    val cols = Array.ofDim[Boolean](4, 4)
+    val squares = Array.ofDim[Boolean](4, 4)
+    rows(1)(1) = true
+    cols(2)(2) = true
+    squares(3)(3) = true
+    val tab = MutableLookupTables(2, rows, cols, squares)
+    rows(1)(1) = false
+    cols(2)(2) = false
+    squares(3)(3) = false
+
+    tab.isRowUsed(1, 0) shouldBe false
+    tab.isRowUsed(2, 1) shouldBe true
+    tab.isColumnUsed(1, 0) shouldBe false
+    tab.isColumnUsed(3, 2) shouldBe true
+    tab.isSquareUsed(1, 0) shouldBe false
+    tab.isSquareUsed(4, 3) shouldBe true
+  }
+
+  it should "allow chaning the used state of numbers and cells" in {
+    val tab = MutableLookupTables(3)
+
+    tab.markUsed(3, 1, 4, used = true)
+    tab.markUsed(9, 8, 8, used = true)
+    tab.markUsed(8, 4, 4, used = true)
+    tab.markUsed(8, 4, 4, used = false)
+
+    tab.isRowUsed(3, 1) shouldBe true
+    tab.isColumnUsed(3, 4) shouldBe true
+    tab.isSquareUsed(9, 8) shouldBe true
+    tab.isRowUsed(8, 4) shouldBe false
+  }
+
+  it should "support transformation to an immutable table" in {
+    val tab = MutableLookupTables(3)
+    tab.markUsed(3, 1, 4, used = true)
+    tab.markUsed(9, 8, 8, used = true)
+    tab.markUsed(8, 4, 4, used = true)
+
+    val tab2 = tab.toImmutable.toMutable
+    tab2.squareSize should be(tab.squareSize)
+    for {v <- 1 to 9
+         i <- 0 until 9
+    } {
+      tab2.isRowUsed(v, i) shouldBe tab.isRowUsed(v, i)
+      tab2.isColumnUsed(v, i) shouldBe tab.isColumnUsed(v, i)
+      tab2.isSquareUsed(v, i) shouldBe tab.isSquareUsed(v, i)
+    }
+  }
+
+  it should "copy arrays when transforming to an immutable table" in {
+    val tab = MutableLookupTables(3)
+    tab.markUsed(3, 1, 1, used = true)
+    val immutableTab = tab.toImmutable
+
+    tab.markUsed(3, 1, 1, used = false)
+    val tab2 = immutableTab.toMutable
+    tab2.isRowUsed(3, 1) shouldBe true
+    tab2.isColumnUsed(3, 1) shouldBe true
+    tab2.isSquareUsed(3, 0) shouldBe true
+  }
+}
